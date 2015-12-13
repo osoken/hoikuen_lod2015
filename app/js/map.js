12f1a4d000fe45b9123afbeee332888a4a855cb9
osoken <osoken.devel@outlook.jp>
2015-12-13 14:25:58 +0900

    modified:   app/js/map.js

--- app/js/map.js
+++ app/js/map.js
@@ -6,6 +6,7 @@ var tile = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 var svgLayer = d3.select(map.getPanes().overlayPane).append('svg').attr('class', 'leaflet-zoom-hide');
 var plotLayer = svgLayer.append('g');
+var lineLayer = svgLayer.append('g');
 
 var updatePosition = function(d)
 {
@@ -25,6 +26,34 @@ var reset = function()
     .style("top", topLeft.y + "px");
 
   plotLayer.attr('transform', 'translate('+ -topLeft.x + ',' + -topLeft.y + ')');
+  lineLayer.attr('transform', 'translate('+ -topLeft.x + ',' + -topLeft.y + ')');
 }
+
+function projectPoint(x, y) {
+  var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+  this.stream.point(point.x, point.y);
+}
+var transform = d3.geo.transform({point: projectPoint}),
+    path = d3.geo.path().projection(transform);
+
+d3.json('data/wards.geojson', function(err,collection)
+{
+  var feature = lineLayer.selectAll("path")
+      .data(collection.features)
+    .enter().append("path");
+  feature.attr("d", path)
+    .attr('fill','rgba(255,255,255,0.1)')
+    .attr('stroke', '#F00')
+    .attr('stroke-width', 5)
+    .on('click', function(d)
+    {
+      console.log(d);
+    });
+  map.on('move', function()
+  {
+    feature.attr('d', path);
+  });
+  reset();
+});
 
 map.on("move", reset);
