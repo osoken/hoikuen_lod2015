92cc2040bef4afc3408b98e894a5c6af46376c6f
U-G01\s-tago <s-tago@AMANATSU.g01.fujitsu.local>
2016-02-03 09:51:51 +0900

    debugged

--- public/js/map.js
+++ public/js/map.js
@@ -84,6 +84,7 @@ d3.json('data/wards.geojson', function(err,collection)
 	})
 	.on('click', function(d)
 	{
+		d3.select("a#mapmes").text("地図上の○印（保育園）か他の区をクリックしてください。");
 		var name = d.properties.name;
 		goo.searchGoo({'keyword':name});
 		if ( name in kulist ) {
